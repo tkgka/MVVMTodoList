c18c403f47aec43b7a8ab537f329e74d5abc82ad
tkgka <ksh17171@gmail.com>
2022-09-18 20:39:01 +0900

    Initial Commit

---
+++ MVVMTodoList/MVVMTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTodoListApp.swift
+//  MVVMTodoList
+//
+//  Created by 김수환 on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
